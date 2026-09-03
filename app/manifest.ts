f89ce86be65591863122531cef419ee99e7d758e
CaptianChill <chillprostx@gmail.com>
2026-09-03 18:02:45 -0500

    Fix web app manifest icon metadata

--- app/manifest.ts
+++ app/manifest.ts
@@ -15,7 +15,7 @@ export default function manifest(): MetadataRoute.Manifest {
         src: "/logo-icon.png",
         sizes: "any",
         type: "image/png",
-        purpose: "any maskable",
+        purpose: "any",
       },
     ],
   };
